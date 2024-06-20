b0312c8149e8e8b37bffd72bd60e257454091fe6
FiddleIdui <88755810+FiddleIdui@users.noreply.github.com>
2024-06-20 12:54:13 +1000

    Shader test
    Followed steps from tutorial
    PS: code outdated
    not sure how to resolve

---
+++ Assets/Shader/ToonShadePP.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+using System;
+
+[Serializable, VolumeComponentMenu("Post-processing/Custom/ToonShadePP")]
+public sealed class ToonShadePP : CustomPostProcessVolumeComponent, IPostProcessComponent
+{
+    [Tooltip("Controls the intensity of the effect.")]
+    [SerializeField] private FloatParameter _posterizeAmount = new FloatParameter(1f);
+
+    Material m_Material;
+
+    public bool IsActive() => m_Material != null && _posterizeAmount.value > 0f;
+
+    // Do not forget to add this post process in the Custom Post Process Orders list (Project Settings > Graphics > HDRP Global Settings).
+    //public override CustomPostProcessInjectionPoint injectionPoint => CustomPostProcessInjectionPoint.AfterPostProcess; --original line--
+    public override CustomPostProcessInjectionPoint injectionPoint => CustomPostProcessInjectionPoint.BeforePostProcess;
+
+    const string kShaderName = "Hidden/Shader/ToonShadePP";
+
+    public override void Setup()
+    {
+        if (Shader.Find(kShaderName) != null)
+            m_Material = new Material(Shader.Find(kShaderName));
+        else
+            Debug.LogError($"Unable to find shader '{kShaderName}'. Post Process Volume ToonShadePP is unable to load.");
+    }
+
+    public override void Render(CommandBuffer cmd, HDCamera camera, RTHandle source, RTHandle destination)
+    {
+        if (m_Material == null)
+            return;
+
+        m_Material.SetFloat("_PosterizeAmount", _posterizeAmount.value);
+        m_Material.SetTexture("_MainTex", source);
+        HDUtils.DrawFullScreen(cmd, m_Material, destination, shaderPassId: 0);
+    }
+
+    public override void Cleanup()
+    {
+        CoreUtils.Destroy(m_Material);
+    }
+}
